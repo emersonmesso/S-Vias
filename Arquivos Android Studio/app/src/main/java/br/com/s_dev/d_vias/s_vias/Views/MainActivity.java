8976fc51b5f9e84ee6e748027c082c4814c9314d
Emerson Santos <Emerson Santos@DESKTOP-0I2UL4P.local>
2018-10-31 11:49:57 -0200

    Adicionando os arquivos do projeto do Android Studio

---
+++ Arquivos Android Studio/app/src/main/java/br/com/s_dev/d_vias/s_vias/Views/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.s_dev.d_vias.s_vias.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.s_dev.d_vias.s_vias.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
