b163c1ab363776ba2640b276dceb80a8ecd8a365
Emerson Santos <Emerson Santos@DESKTOP-0I2UL4P.local>
2018-10-31 10:23:04 -0200

    Adicionando os arquivos do projeto do Android Studio

---
+++ Arquivos Android Studio/app/build/generated/source/buildConfig/debug/br/com/s_dev/s_vias/s_vias/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.s_dev.s_vias.s_vias;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.com.s_dev.s_vias.s_vias";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
